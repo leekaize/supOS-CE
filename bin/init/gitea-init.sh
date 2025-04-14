4a2ead16a364c5cd12920b60fef34cf926491038
simonwindy <144756616+simonwindy@users.noreply.github.com>
2025-04-14 15:42:22 +0800

    Update gitea-init.sh
    change default Gitea user email to match the default user in Keycloak

--- bin/init/gitea-init.sh
+++ bin/init/gitea-init.sh
@@ -8,7 +8,7 @@ source $SCRIPT_DIR/../global/log.sh
 source $SCRIPT_DIR/../../.env
 
 echo ">> start to init gitea ..."
-docker exec --user 1000 gitea sh -c "gitea admin user create --admin --username supos --password Supos@1304 --email supos@example.com" \
+docker exec --user 1000 gitea sh -c "gitea admin user create --admin --username supos --password Supos@1304 --email supos@supos.com" \
 || if [ "$1" == "--verbose" ]; then warn "Failed to create admin user"; fi
 
 docker exec --user 1000 gitea sh -c "gitea admin auth add-oauth --name $OAUTH_CLIENT_NAME --provider openidConnect --key $OAUTH_CLIENT_ID \
