4264d1e3d111f16e90cb669742d4863802a1fc09
simonwindy <144756616+simonwindy@users.noreply.github.com>
2025-04-28 09:51:14 +0800

    Update portainer-init.sh
    better compatibility with HTTPS/443 situation

--- bin/init/portainer-init.sh
+++ bin/init/portainer-init.sh
@@ -8,6 +8,15 @@ source $SCRIPT_DIR/../global/log.sh
 source $SCRIPT_DIR/../../.env
 
 echo ">> start to init portainer OAuth ..."
+
+# Determine the port string to use
+PORT_STRING=""
+if [[ ("$ENTRANCE_PROTOCOL" == "https" && "$ENTRANCE_PORT" == "443") || ("$ENTRANCE_PROTOCOL" == "http" && "$ENTRANCE_PORT" == "80") ]]; then
+  PORT_STRING=""
+else
+  PORT_STRING=":$ENTRANCE_PORT"
+fi
+
 PORTAINER_JWT=`docker exec nodered curl -skX POST https://portainer:9443/api/auth      -H "Content-Type: application/json"      -d '{"username": "admin", "password": "adminpassword"}' | awk -F'"' '/jwt/ {print $4}'` && echo "Successfully got Portainer JWT"\
 || if [ "$1" == "--verbose" ]; then warn "Failed to obtain JWT from Portainer"; fi
 
@@ -20,14 +29,14 @@ docker exec nodered curl -skX PUT "https://portainer:9443/api/settings" \
    -d "{
      \"authenticationMethod\": 3,
      \"oauthSettings\": {
-       \"AccessTokenURI\": \"$ENTRANCE_PROTOCOL://$ENTRANCE_DOMAIN:$ENTRANCE_PORT/keycloak/home/auth/realms/supos/protocol/openid-connect/token\",
+       \"AccessTokenURI\": \"$ENTRANCE_PROTOCOL://$ENTRANCE_DOMAIN${PORT_STRING}/keycloak/home/auth/realms/supos/protocol/openid-connect/token\",
        \"AuthStyle\": 0,
-       \"AuthorizationURI\": \"$ENTRANCE_PROTOCOL://$ENTRANCE_DOMAIN:$ENTRANCE_PORT/keycloak/home/auth/realms/supos/protocol/openid-connect/auth\",
+       \"AuthorizationURI\": \"$ENTRANCE_PROTOCOL://$ENTRANCE_DOMAIN${PORT_STRING}/keycloak/home/auth/realms/supos/protocol/openid-connect/auth\",
        \"ClientID\": \"$OAUTH_CLIENT_ID\",
        \"ClientSecret\": \"$OAUTH_CLIENT_SECRET\",
        \"OAuthAutoCreateUsers\": true,
-       \"RedirectURI\": \"$ENTRANCE_PROTOCOL://$ENTRANCE_DOMAIN:$ENTRANCE_PORT/portainer/home/\",
-       \"ResourceURI\": \"$ENTRANCE_PROTOCOL://$ENTRANCE_DOMAIN:$ENTRANCE_PORT/keycloak/home/auth/realms/supos/protocol/openid-connect/userinfo\",
+       \"RedirectURI\": \"$ENTRANCE_PROTOCOL://$ENTRANCE_DOMAIN${PORT_STRING}/portainer/home/\",
+       \"ResourceURI\": \"$ENTRANCE_PROTOCOL://$ENTRANCE_DOMAIN${PORT_STRING}/keycloak/home/auth/realms/supos/protocol/openid-connect/userinfo\",
        \"SSO\": true,
        \"UserIdentifier\":\"preferred_username\",
        \"Scopes\":\"openid\"
