e62db304c114ab457483061c7179347d65df1a3f
shimu <1239380544@qq.com>
2025-06-17 16:22:32 +0800

    update eventflow-init.sh

--- bin/startup.sh
+++ bin/startup.sh
@@ -171,6 +171,7 @@ if [ "$OS_RESOURCE_SPEC" == "1" ]; then
   DOCKER_COMPOSE_FILE=$SCRIPT_DIR/../docker-compose-4c8g.yml
 fi
 
+echo "Start creating volumes"
 # 创建volumes目录, 将mount目录迁移到volumes目录
 if [ -d "$VOLUMES_PATH" ] && [ "$(ls -A $VOLUMES_PATH)" ]; then
   info "stop services and apps..."
@@ -191,7 +192,7 @@ fi
 
 if docker compose --env-file $SCRIPT_DIR/../.env --env-file $SCRIPT_DIR/../.env.tmp --project-name supos $command -f $DOCKER_COMPOSE_FILE up -d && \
    source $SCRIPT_DIR/init/init-nodered.sh $1 && \
-   #bash $SCRIPT_DIR/init/node-red-init.sh 1889 eventflow $1 && \
+   source $SCRIPT_DIR/init/eventflow-init.sh 1889 eventflow $1 && \
    source $SCRIPT_DIR/init/init-minio.sh $1 > /dev/null 2>&1 && \
    source $SCRIPT_DIR/init/init-portainer.sh; then
 
